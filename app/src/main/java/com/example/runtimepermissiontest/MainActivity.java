0980c080f59e488f85c1e8223aebdd1000efc447
G-WS <3355139577@qq.com>
2022-08-13 11:11:39 +0800

    0813

--- app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
+++ app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
@@ -1,14 +1,107 @@
 package com.example.runtimepermissiontest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
+    List<String> contacksList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView listView = (ListView) findViewById(R.id.contacts_view);
+        adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, contacksList);
+        listView.setAdapter(adapter);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                    Manifest.permission.READ_CONTACTS}, 1);
+
+        } else {
+            readContacts();
+        }
+
+//        Button makecall = (Button) findViewById(R.id.make_call);
+//        makecall.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+//                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+//                            Manifest.permission.CALL_PHONE}, 1
+//                    );
+//                } else {
+//                    call();
+//                }
+//            }
+//
+//        });
     }
+
+    private void readContacts() {
+        Cursor cursor = null;
+        try {
+            cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+            if (cursor != null) {
+                while (cursor.moveToNext()) {
+                    @SuppressLint("Range") String displayName = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    @SuppressLint("Range") String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    contacksList.add(displayName + "\n" + number);
+                }
+                adapter.notifyDataSetChanged();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    readContacts();
+//                    call();
+                } else {
+                    Toast.makeText(MainActivity.this, "you denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+
+//    private void call() {
+//        try {
+//            Intent intent = new Intent(Intent.ACTION_CALL);
+//            intent.setData(Uri.parse("tel:1008611"));
+//            startActivity(intent);
+//        } catch (SecurityException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
---
+++ app/src/main/java/com/example/runtimepermissiontest/MyProvider.java
@@ -0,0 +1,72 @@
+package com.example.runtimepermissiontest;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyProvider extends ContentProvider {
+    public static final int TABLE1_DIR = 0;
+    public static final int TABLE1_ITEM = 1;
+    public static final int TABLE2_DIR = 1;
+    public static final int TABLE2_ITEM = 3;
+    private static UriMatcher uriMatcher;
+
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("com.example.app.provider", "table1", TABLE1_DIR);
+        uriMatcher.addURI("com.example.app.provider", "table1/#", TABLE1_ITEM);
+        uriMatcher.addURI("com.example.app.provider", "table2", TABLE2_DIR);
+        uriMatcher.addURI("com.example.app.provider", "table2/#", TABLE2_ITEM);
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+//        switch (uriMatcher.match(uri)) {
+//            case TABLE1_DIR:
+//                break;
+//            case TABLE1_ITEM:
+//                break;
+//
+//            case TABLE2_DIR:
+//                break;
+//            case TABLE2_ITEM:
+//                break;
+//            default:
+//                break;
+//        }
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
